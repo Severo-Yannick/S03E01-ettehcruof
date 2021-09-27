ac1c4d2e701774b84c886925f3200f3919609a20
Yannick Severo <severoyannick@gmail.com>
2021-09-27 17:24:22 +0200

    Trouver le moyen de répéter ce procédé jusqu'à trouver le bon nombre.

--- app.js
+++ app.js
@@ -9,33 +9,45 @@ const interface = readline.createInterface({
   output: process.stdout
 });
 
+// Nombre aléatoire mini
+let minBoundary = 1;
+// Nombre aléatoire maxi
+let maxBoundary = 101;
+
 // Générer un nombre aléatoire
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
-let proposition = (getRandomInt(1, 101));
+let proposition = (getRandomInt(minBoundary, maxBoundary));
 
 // Question posée au user
 console.log(`Est-ce que le nombre est ${proposition} ?`);
 
 // Décrit quoi faire au user quand l'évent aura lieu
 interface.on('line', (input) => {
-  console.log(`Vous avez saisie la valeur ==> ${input}`);
-  // Le nombre est trouvé
+  console.log(`Vous avez saisie ==> ${input}`);
   // Le nombre est supérieur
-  if(input == '+' || 'plus' || 'C\’est plus') {
-    console.log(`On va proposer plus grand`);
+  if(input === '+' || input === 'plus' || input === 'C\’est plus') {
+    minBoundary = proposition ++;
+    proposition = (getRandomInt(minBoundary, maxBoundary));
+    console.log(`Vous dites plus peut-être ${proposition}`);
+    return;
   }
   // Le nombre est inférieur
-  else if(input == '-' || 'moins' || 'C\’est moins') {
-    console.log(`On va proposer plus petit`);
+  else if(input == '-' || input === 'moins' || input === 'C\’est moins') {
+    maxBoundary = proposition --;
+    proposition = (getRandomInt(minBoundary, maxBoundary));
+    console.log(`Vous dites moins peut-être ${proposition}`);
+    return;
   }
-  else if(parseInt(input) === proposition) {
+  // Le nombre est trouvé
+  else if(input == '=' || input === 'gagné' || input === 'oui') {
     console.log('Gagné !');
-    return
+    return;
   }
+  // La saisie n'est pas conforme
   else {
     console.log('Désolé je ne comprends pas');
   }
