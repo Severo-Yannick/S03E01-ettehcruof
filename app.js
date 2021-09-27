33920cdf6f89b06ffef24ecc706b3dbf8327bd23
Yannick Severo <severoyannick@gmail.com>
2021-09-27 13:04:56 +0200

    Créer un programme qui pose une question à l'utilisateur

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// TODO
+// Programme qui pose une question à l'utilisateur et affiche sa réponse dans la console
+
+// Module readline fournit par le runtime node
+const readline = require('readline');
+
+// Création de l'interface
+const interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+// Question posée au user
+console.log('Est-ce que le nombre est 50 ?');
+
+// Décrit quoi faire au user quand l'évent aura lieu
+interface.on('line', () => {
+  console.log('Vous-avez rentré une valeur');
+});
